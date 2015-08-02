08426af1b873cf197084c01ad2ab27c95942cd22
Ryo Ota <nwtgck@gmail.com>
2015-08-03 01:28:03 +0900

    [change] change readme

--- demo.rb
+++ demo.rb
@@ -2,29 +2,24 @@ require 'js_obj'
 
 user = {
 	id: 1001,
+	name: 'user1',
 	position: {
 		x: 10.5,
 		y: -1.2
 	}
 }
 
-# Get Property
+# Get
+puts user.id #=> 1001
+puts user.position.x #=> 10.5
 
-puts user.id
-#=> 1001
-puts user.position.x
-#=> 10.5
+# Set
+user.name = "Jason"
+puts user.name #=> "Jason"
 
-# Add property
-
-user.name = "user1"
-puts user.name
-#=> user1
-
+# Add
 user.position.z = 9.8
-puts user.position.z
-#=> 9.8
+puts user.position.z #=> 9.8
 
 # Usual way
-puts user[:id]
-#=> 1001
+puts user[:id] #=> 1001
